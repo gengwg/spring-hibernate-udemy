3c5efb251f39774d16b2e9fd8494b02b134cde89
Weigang Geng <wgeng@walmartlabs.com>
2019-02-20 11:19:57 -0800

    refactor using interface

---
+++ spring/src/com/luv2code/springdemo/Coach.java
@@ -0,0 +1,8 @@
+package com.luv2code.springdemo;
+
+public interface Coach {
+	
+	// interface only specify interface, not implementing them
+	public String getDailyWorkout();
+
+}
--- spring/src/com/luv2code/springdemo/MyApp.java
+++ spring/src/com/luv2code/springdemo/MyApp.java
@@ -5,7 +5,8 @@ public class MyApp {
 	public static void main(String[] args) {
 
 		// create the object
-		BaseballCoach theCoach = new BaseballCoach();
+		// use the interface
+		Coach theCoach = new BaseballCoach();
 		
 		// use the object
 		System.out.println(theCoach.getDailyWorkout());
